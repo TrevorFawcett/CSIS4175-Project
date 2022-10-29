c0d756b61659de61b5049157a0d32c748a06a6ce
vorobyovayana <vorobyovayana@yandex.ru>
2022-10-29 15:03:02 -0700

    "Account Navigation Fragment layout created (not final)"

---
+++ CSIS4175choose2help/app/src/main/java/com/example/csis4175choose2help/AccountFragment.java
@@ -0,0 +1,30 @@
+package com.example.csis4175choose2help;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AccountFragment extends Fragment {
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+}
