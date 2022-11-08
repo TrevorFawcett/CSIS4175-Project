f14d198b0b1f981449d06be11548eba39790d04b
TrevorFawcett <49043544+TrevorFawcett@users.noreply.github.com>
2022-11-08 13:38:13 -0800

    new project

---
+++ choose2help4175/app/src/main/java/com/example/choose2help4175/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.choose2help4175.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
