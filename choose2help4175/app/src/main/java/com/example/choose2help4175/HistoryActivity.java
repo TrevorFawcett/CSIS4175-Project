d3c524822783cc7d98a65dc9230db21ee32b3e55
vorobyovayana <vorobyovayana@yandex.ru>
2022-11-09 07:29:31 -0800

    "Added donation history activity + modified the navigation drawer titles"

---
+++ choose2help4175/app/src/main/java/com/example/choose2help4175/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.choose2help4175;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.choose2help4175.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
