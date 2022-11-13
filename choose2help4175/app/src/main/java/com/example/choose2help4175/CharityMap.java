f94f1b260fa4ecbdcd4a81910be2c41b0ae31545
jerikohh <109439536+jerikohh@users.noreply.github.com>
2022-11-13 01:41:54 -0800

    Added charity map layout

---
+++ choose2help4175/app/src/main/java/com/example/choose2help4175/CharityMap.java
@@ -0,0 +1,14 @@
+package com.example.choose2help4175;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CharityMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_charity_map);
+    }
+}
