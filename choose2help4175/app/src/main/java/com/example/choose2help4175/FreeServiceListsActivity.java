e642c3f3f22e548387151226a387feb672852ebc
vorobyovayana <vorobyovayana@yandex.ru>
2022-12-04 20:46:13 -0800

    "Fix after merge"

--- choose2help4175/app/src/main/java/com/example/choose2help4175/FreeServiceListsActivity.java
+++ choose2help4175/app/src/main/java/com/example/choose2help4175/FreeServiceListsActivity.java
@@ -104,12 +104,16 @@ public class FreeServiceListsActivity extends BaseActivity implements FreeServic
 //            }
 //        });
 
+        btnFSBringList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
                 loadData();
                 btnFSBringList.setText("Reload the list");
                 bringData = false;
             }
         });
+
 
     }
 
